b222a257c690c3609b6c0ab523054801c5520363
Peter Keating <peter@moov2.com>
2019-01-27 21:12:25 +0000

    Configure project as orchard core module

---
+++ Manifest.cs
@@ -0,0 +1,15 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "Gallery",
+    Author = "Moov2",
+    Website = "https://moov2.com",
+    Version = "0.0.1"
+)]
+
+[assembly: Feature(
+    Id = "Moov2.OrchardCore.Gallery",
+    Name = "Gallery",
+    Description = "Display a collection of images and videos",
+    Category = "Content"
+)]
