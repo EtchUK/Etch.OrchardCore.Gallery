871943ba2f6a9f5c9bd6264ddda76aa2b97e152d
mobinzk <mobinzk@gmail.com>
2019-03-04 10:53:03 +0000

    Add gallery markup to get from JSON
    Example json is:
    {[“Url”: “Image URL”, “Thumb”: “Image thump url” ,“type”: 1, Title: “image URL”]}

--- Models/GalleryPartItem.cs
+++ Models/GalleryPartItem.cs
@@ -1,7 +1,9 @@
 ﻿namespace Moov2.OrchardCore.Gallery.Models {
 
     public class GalleryPartItem {
-        public string Url { get; set; }
         public int type { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Thumb { get; set; }
     }
 }
