b844e45dcce6dc207b7f032ca4048934f93d4c9c
mobinzk <mobinzk@gmail.com>
2019-03-14 17:03:53 +0000

    Fix semantic typo error

--- Models/GalleryPartItem.cs
+++ Models/GalleryPartItem.cs
@@ -1,7 +1,7 @@
 ﻿namespace Moov2.OrchardCore.Gallery.Models {
 
     public class GalleryPartItem {
-        public int type { get; set; }
+        public int Type { get; set; }
         public string Url { get; set; }
         public string Title { get; set; }
         public string Thumb { get; set; }
