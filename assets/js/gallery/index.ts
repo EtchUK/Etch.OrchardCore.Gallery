d4f4c4382346b3816ca6a9f6045d030a3ba59129
mobinzk <mobinzk@gmail.com>
2019-03-15 12:22:48 +0000

    Convert gallary module to compile using webpack
    Running `npm run build` compiles both js files for frontend and backend admin

---
+++ assets/js/gallery/index.ts
@@ -0,0 +1,37 @@
+﻿/**
+ * Gallery lightbox
+ */
+
+import 'lightgallery.js';
+import 'lg-video.js';
+
+const lightGallery = (window as any).lightGallery;
+
+const gallery = ($el: Element): void => {
+    // Handle single link
+    if ($el instanceof HTMLAnchorElement) {
+        lightGallery($el, {
+            selector: 'this',
+            videoMaxWidth: '100%',
+        });
+
+        return;
+    }
+
+    // Handle multiple images/links
+    lightGallery($el, {
+        selector: 'a',
+        videoMaxWidth: '100%',
+    });
+};
+
+const init = (): void => {
+    const selector = '.js-gallery';
+    let $elements = document.querySelectorAll(selector);
+
+    for (let i = 0; i < $elements.length; ++i) {
+        gallery($elements[i]);
+    }
+};
+
+init();
