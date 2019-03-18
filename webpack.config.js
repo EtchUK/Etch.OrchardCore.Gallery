fdf0621591963c0885b410a40ae2827d1abb49b9
mobinzk <mobinzk@gmail.com>
2019-03-18 14:43:14 +0000

    Add compiling sass to webpack
    To build js/css use `npm run build` to build for distribution. And `npm run dev` to build and watch for development

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
     entry: {
         'admin': './Assets/js/admin/index.ts',
-        'gallery': './Assets/js/gallery/index.ts'
+        'gallery': './Assets/js/gallery/index.ts',
+        'styles': './Assets/css/styles.scss'
     },
     mode: 'development',
     module: {
@@ -13,6 +16,22 @@ module.exports = {
                 use: 'ts-loader',
                 exclude: /node_modules/,
             },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader?-url',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function () {
+                                return [autoprefixer('last 1 version', 'ie 10')]
+                            }
+                        }
+                    },
+                    'sass-loader'
+                ]
+            }
         ],
     },
     resolve: {
@@ -22,4 +41,9 @@ module.exports = {
         filename: '[name].js',
         path: path.resolve(__dirname, './wwwroot/Scripts'),
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '../Styles/styles.css'
+        })
+    ]
 };
