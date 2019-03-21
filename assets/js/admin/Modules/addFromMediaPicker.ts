f2135e6b7adc4103d07569d98c10a927d59075c2
mobinzk <mobinzk@gmail.com>
2019-03-21 14:44:16 +0000

    Add validation to only allow users to select image from media picker
    This prevent users to add other file formats, e.g. pdf

--- assets/js/admin/Modules/addFromMediaPicker.ts
+++ assets/js/admin/Modules/addFromMediaPicker.ts
@@ -46,23 +46,35 @@ export const addFromMediaPicker = (): GalleryModel => {
         const $okButton = $modal.find('button[data-accept="model"]').first();
         $okButton.on('click', () => {
             const mediaApp = (window as any).mediaApp;
-
             const $jsonInput = $('.gallery > .' + id + '-MediaItems').first();
-
             const galleryJsonModel = new GalleryJsonModel($jsonInput);
+            var galleryPartItems = Array<GalleryPartItem>();
 
-            const galleryPartItems = Array<GalleryPartItem>();
+            var isValidImage = true;
 
             mediaApp.selectedMedias.forEach((media: any) => {
+                // Check if user has selected anything other than image then show an error
+                if (!media.mime.startsWith('image')) {
+                    alert('You only allow to select image.');
+
+                    galleryPartItems = [];
+                    isValidImage = false;
+                    return;
+                }
+
                 const galleryPartItem = new GalleryPartItem(
                     GalleryPartType.Image,
                     media.url,
-                    '',
+                    media.name,
                     media.url
                 );
 
                 galleryPartItems.push(galleryPartItem);
             });
+
+            if (!isValidImage) {
+                return;
+            }
 
             galleryJsonModel.addAll(galleryPartItems);
 
