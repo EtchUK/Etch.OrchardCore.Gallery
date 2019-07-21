8ce655fc23ab8d71f0b1d9adad6fcd30af333cf9
Peter Keating <peter@moov2.com>
2019-07-21 22:57:11 +0100

    Only load front-end assets & render list of gallery has items

--- ViewModels/GalleryPartDisplayViewModel.cs
+++ ViewModels/GalleryPartDisplayViewModel.cs
@@ -1,8 +1,14 @@
 ﻿using Etch.OrchardCore.Gallery.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Etch.OrchardCore.Gallery.ViewModels {
     public class GalleryPartDisplayViewModel {
         public List<GalleryPartItem> MediaItems { get; set; }
+
+        public bool HasMediaItems
+        {
+            get { return MediaItems != null && MediaItems.Any(); }
+        }
     }
 }
