0331d8610cdeff25f57525df89c02db852b6cb96
Adam Burt <adam.burt@etchuk.com>
2020-07-15 08:30:23 +0100

    Order properties alphabetically in GalleryPartItem class

--- Models/GalleryPartItem.cs
+++ Models/GalleryPartItem.cs
@@ -1,10 +1,10 @@
 ﻿namespace Etch.OrchardCore.Gallery.Models {
 
     public class GalleryPartItem {
+        public string Thumb { get; set; }
         public string Title { get; set; }
         public int Type { get; set; }
         public string TypeName { get; set; }
         public string Url { get; set; }
-        public string Thumb { get; set; }
     }
 }
