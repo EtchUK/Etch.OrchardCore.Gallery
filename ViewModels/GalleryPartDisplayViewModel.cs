31883e44cf1826614d62e484b47e22561ddd0d5f
Peter Keating <pete.keating@etchuk.com>
2021-08-12 16:00:04 +0100

    Include properties contain part & content item on view model for gallery part display

--- ViewModels/GalleryPartDisplayViewModel.cs
+++ ViewModels/GalleryPartDisplayViewModel.cs
@@ -1,9 +1,14 @@
-﻿using System.Linq;
+﻿using Etch.OrchardCore.Gallery.Models;
+using OrchardCore.ContentManagement;
+using System.Linq;
 
 namespace Etch.OrchardCore.Gallery.ViewModels
 {
-    public class GalleryPartDisplayViewModel {
+    public class GalleryPartDisplayViewModel 
+    {
+        public ContentItem ContentItem { get; set; }
         public GalleryPartItemViewModel[] MediaItems { get; set; }
+        public GalleryPart Part { get; set; }
 
         public bool HasMediaItems
         {
