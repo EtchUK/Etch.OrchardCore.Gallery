482af172fee0bece5a6db028947feebd25815d6a
Mehdi Miah <Mehdi.Miah@etchuk.com>
2022-06-13 10:52:07 +0100

    Upgrade to orchard core 1.4.0

--- Manifest.cs
+++ Manifest.cs
@@ -4,7 +4,7 @@ using OrchardCore.Modules.Manifest;
     Name = "Gallery",
     Author = "Etch UK",
     Website = "https://etchuk.com",
-    Version = "1.2.0"
+    Version = "1.3.0"
 )]
 
 [assembly: Feature(
